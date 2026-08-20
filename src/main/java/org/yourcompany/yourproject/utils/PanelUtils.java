854bbaa88064976c9a99102fe8fa165e0ca59090
James Anthony Arcenas <jamesanthony.arcenas1025@gmail.com>
2026-08-20 22:19:03 +0800

    set the title at the center of the panel

--- src/main/java/org/yourcompany/yourproject/utils/PanelUtils.java
+++ src/main/java/org/yourcompany/yourproject/utils/PanelUtils.java
@@ -1,5 +1,6 @@
 package org.yourcompany.yourproject.utils;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
@@ -8,28 +9,38 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import javax.swing.SwingConstants;
 
 public class PanelUtils{
     public static JPanel displayPanel(Dimension d, String title, String description){
         JPanel bPanel = new JPanel(); // wrapper to control the size of the display panel
         JPanel dPanel = new JPanel();
+        JPanel header = new JPanel();
         JButton closeButton = new JButton();
         JLabel t = new JLabel();
         JTextArea desc = new JTextArea();
-
         bPanel.setLayout(new FlowLayout());
 
 
         //Display Panel Configuration
-        dPanel.setBackground(Color.BLUE);
-        dPanel.add(closeButton);
-        dPanel.add(t);
-        dPanel.add(desc);
+        dPanel.setLayout(new BorderLayout());
+        dPanel.setBackground(Color.GRAY);
+        dPanel.add(header, BorderLayout.NORTH);
+        header.setLayout(new BorderLayout());
+        header.setOpaque(false);
+        //Title Configuration
+        t.setHorizontalAlignment(SwingConstants.CENTER);
+        t.setOpaque(false);
+        header.add(t, BorderLayout.CENTER);
+
+        header.add(closeButton, BorderLayout.EAST);
+        
 
 
         //Title and Info
         t.setText(title);
         desc.setText(description);
+
 
         closeButton.addActionListener(e-> {
             if(bPanel.isVisible() == true){
