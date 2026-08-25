412fef99c3d88bb059b70df0c328e96c8d7151df
James Anthony Arcenas <jamesanthony.arcenas1025@gmail.com>
2026-08-25 23:21:17 +0800

    add all switch cases

--- src/main/java/org/yourcompany/yourproject/utils/InformationUtils.java
+++ src/main/java/org/yourcompany/yourproject/utils/InformationUtils.java
@@ -21,11 +21,234 @@ public class InformationUtils{
             
 
             switch (index) {
-                case  1:
-                    JPanel panel = PanelUtils.displayPanel(panelSize, "Fortran", "desc", "src/main/java/org/yourcompany/yourproject/resources/fortran logo.jpeg");
+                case 1: {
+                    JPanel panel = PanelUtils.displayPanel(panelSize, "Fortran I", "desc", "src/main/java/org/yourcompany/yourproject/resources/fortran logo.jpeg");
                     panel.setBounds(bounds);
                     panel.setVisible(true);
                     return panel;
+                }
+                case 2: {
+                    JPanel panel = PanelUtils.displayPanel(panelSize, "Fortran II", "desc", "src/main/java/org/yourcompany/yourproject/resources/logo.jpeg");
+                    panel.setBounds(bounds);
+                    panel.setVisible(true);
+                    return panel;
+                }
+                case 3: {
+                    JPanel panel = PanelUtils.displayPanel(panelSize, "Fortran IV", "desc", "src/main/java/org/yourcompany/yourproject/resources/logo.jpeg");
+                    panel.setBounds(bounds);
+                    panel.setVisible(true);
+                    return panel;
+                }
+                case 4: {
+                    JPanel panel = PanelUtils.displayPanel(panelSize, "Fortran 77", "desc", "src/main/java/org/yourcompany/yourproject/resources/logo.jpeg");
+                    panel.setBounds(bounds);
+                    panel.setVisible(true);
+                    return panel;
+                }
+                case 5: {
+                    JPanel panel = PanelUtils.displayPanel(panelSize, "Fortran 90", "desc", "src/main/java/org/yourcompany/yourproject/resources/logo.jpeg");
+                    panel.setBounds(bounds);
+                    panel.setVisible(true);
+                    return panel;
+                }
+                case 6: {
+                    JPanel panel = PanelUtils.displayPanel(panelSize, "Algol 58", "desc", "src/main/java/org/yourcompany/yourproject/resources/logo.jpeg");
+                    panel.setBounds(bounds);
+                    panel.setVisible(true);
+                    return panel;
+                }
+                case 7: {
+                    JPanel panel = PanelUtils.displayPanel(panelSize, "Algol 60", "desc", "src/main/java/org/yourcompany/yourproject/resources/logo.jpeg");
+                    panel.setBounds(bounds);
+                    panel.setVisible(true);
+                    return panel;
+                }
+                case 8: {
+                    JPanel panel = PanelUtils.displayPanel(panelSize, "Algol W", "desc", "src/main/java/org/yourcompany/yourproject/resources/logo.jpeg");
+                    panel.setBounds(bounds);
+                    panel.setVisible(true);
+                    return panel;
+                }
+                case 9: {
+                    JPanel panel = PanelUtils.displayPanel(panelSize, "Algol 68", "desc", "src/main/java/org/yourcompany/yourproject/resources/logo.jpeg");
+                    panel.setBounds(bounds);
+                    panel.setVisible(true);
+                    return panel;
+                }
+                case 10: {
+                    JPanel panel = PanelUtils.displayPanel(panelSize, "Pascal", "desc", "src/main/java/org/yourcompany/yourproject/resources/logo.jpeg");
+                    panel.setBounds(bounds);
+                    panel.setVisible(true);
+                    return panel;
+                }
+                case 11: {
+                    JPanel panel = PanelUtils.displayPanel(panelSize, "Modula-2", "desc", "src/main/java/org/yourcompany/yourproject/resources/logo.jpeg");
+                    panel.setBounds(bounds);
+                    panel.setVisible(true);
+                    return panel;
+                }
+                case 12: {
+                    JPanel panel = PanelUtils.displayPanel(panelSize, "Modula-3", "desc", "src/main/java/org/yourcompany/yourproject/resources/logo.jpeg");
+                    panel.setBounds(bounds);
+                    panel.setVisible(true);
+                    return panel;
+                }
+                case 13: {
+                    JPanel panel = PanelUtils.displayPanel(panelSize, "Oberon", "desc", "src/main/java/org/yourcompany/yourproject/resources/logo.jpeg");
+                    panel.setBounds(bounds);
+                    panel.setVisible(true);
+                    return panel;
+                }
+                case 14: {
+                    JPanel panel = PanelUtils.displayPanel(panelSize, "Ada 83", "desc", "src/main/java/org/yourcompany/yourproject/resources/logo.jpeg");
+                    panel.setBounds(bounds);
+                    panel.setVisible(true);
+                    return panel;
+                }
+                case 15: {
+                    JPanel panel = PanelUtils.displayPanel(panelSize, "Ada 95", "desc", "src/main/java/org/yourcompany/yourproject/resources/logo.jpeg");
+                    panel.setBounds(bounds);
+                    panel.setVisible(true);
+                    return panel;
+                }
+                case 16: {
+                    JPanel panel = PanelUtils.displayPanel(panelSize, "Simula I", "desc", "src/main/java/org/yourcompany/yourproject/resources/logo.jpeg");
+                    panel.setBounds(bounds);
+                    panel.setVisible(true);
+                    return panel;
+                }
+                case 17: {
+                    JPanel panel = PanelUtils.displayPanel(panelSize, "Simula 67", "desc", "src/main/java/org/yourcompany/yourproject/resources/logo.jpeg");
+                    panel.setBounds(bounds);
+                    panel.setVisible(true);
+                    return panel;
+                }
+                case 18: {
+                    JPanel panel = PanelUtils.displayPanel(panelSize, "Smalltalk 80", "desc", "src/main/java/org/yourcompany/yourproject/resources/logo.jpeg");
+                    panel.setBounds(bounds);
+                    panel.setVisible(true);
+                    return panel;
+                }
+                case 19: {
+                    JPanel panel = PanelUtils.displayPanel(panelSize, "Eiffel", "desc", "src/main/java/org/yourcompany/yourproject/resources/logo.jpeg");
+                    panel.setBounds(bounds);
+                    panel.setVisible(true);
+                    return panel;
+                }
+                case 20: {
+                    JPanel panel = PanelUtils.displayPanel(panelSize, "APL", "desc", "src/main/java/org/yourcompany/yourproject/resources/logo.jpeg");
+                    panel.setBounds(bounds);
+                    panel.setVisible(true);
+                    return panel;
+                }
+                case 21: {
+                    JPanel panel = PanelUtils.displayPanel(panelSize, "BASIC", "desc", "src/main/java/org/yourcompany/yourproject/resources/logo.jpeg");
+                    panel.setBounds(bounds);
+                    panel.setVisible(true);
+                    return panel;
+                }
+                case 22: {
+                    JPanel panel = PanelUtils.displayPanel(panelSize, "QuickBASIC", "desc", "src/main/java/org/yourcompany/yourproject/resources/logo.jpeg");
+                    panel.setBounds(bounds);
+                    panel.setVisible(true);
+                    return panel;
+                }
+                case 23: {
+                    JPanel panel = PanelUtils.displayPanel(panelSize, "Visual BASIC", "desc", "src/main/java/org/yourcompany/yourproject/resources/logo.jpeg");
+                    panel.setBounds(bounds);
+                    panel.setVisible(true);
+                    return panel;
+                }
+                case 24: {
+                    JPanel panel = PanelUtils.displayPanel(panelSize, "FLOW-MATIC", "desc", "src/main/java/org/yourcompany/yourproject/resources/logo.jpeg");
+                    panel.setBounds(bounds);
+                    panel.setVisible(true);
+                    return panel;
+                }
+                case 25: {
+                    JPanel panel = PanelUtils.displayPanel(panelSize, "COBOL", "desc", "src/main/java/org/yourcompany/yourproject/resources/logo.jpeg");
+                    panel.setBounds(bounds);
+                    panel.setVisible(true);
+                    return panel;
+                }
+                case 26: {
+                    JPanel panel = PanelUtils.displayPanel(panelSize, "PL/I", "desc", "src/main/java/org/yourcompany/yourproject/resources/logo.jpeg");
+                    panel.setBounds(bounds);
+                    panel.setVisible(true);
+                    return panel;
+                }
+                case 27: {
+                    JPanel panel = PanelUtils.displayPanel(panelSize, "CPL", "desc", "src/main/java/org/yourcompany/yourproject/resources/logo.jpeg");
+                    panel.setBounds(bounds);
+                    panel.setVisible(true);
+                    return panel;
+                }
+                case 28: {
+                    JPanel panel = PanelUtils.displayPanel(panelSize, "BCPL", "desc", "src/main/java/org/yourcompany/yourproject/resources/logo.jpeg");
+                    panel.setBounds(bounds);
+                    panel.setVisible(true);
+                    return panel;
+                }
+                case 29: {
+                    JPanel panel = PanelUtils.displayPanel(panelSize, "B", "desc", "src/main/java/org/yourcompany/yourproject/resources/logo.jpeg");
+                    panel.setBounds(bounds);
+                    panel.setVisible(true);
+                    return panel;
+                }
+                case 30: {
+                    JPanel panel = PanelUtils.displayPanel(panelSize, "C", "desc", "src/main/java/org/yourcompany/yourproject/resources/logo.jpeg");
+                    panel.setBounds(bounds);
+                    panel.setVisible(true);
+                    return panel;
+                }
+                case 31: {
+                    JPanel panel = PanelUtils.displayPanel(panelSize, "C++", "desc", "src/main/java/org/yourcompany/yourproject/resources/logo.jpeg");
+                    panel.setBounds(bounds);
+                    panel.setVisible(true);
+                    return panel;
+                }
+                case 32: {
+                    JPanel panel = PanelUtils.displayPanel(panelSize, "ANSI C", "desc", "src/main/java/org/yourcompany/yourproject/resources/logo.jpeg");
+                    panel.setBounds(bounds);
+                    panel.setVisible(true);
+                    return panel;
+                }
+                case 33: {
+                    JPanel panel = PanelUtils.displayPanel(panelSize, "Java", "desc", "src/main/java/org/yourcompany/yourproject/resources/logo.jpeg");
+                    panel.setBounds(bounds);
+                    panel.setVisible(true);
+                    return panel;
+                }
+                case 34: {
+                    JPanel panel = PanelUtils.displayPanel(panelSize, "LISP", "desc", "src/main/java/org/yourcompany/yourproject/resources/logo.jpeg");
+                    panel.setBounds(bounds);
+                    panel.setVisible(true);
+                    return panel;
+                }
+                case 35: {
+                    JPanel panel = PanelUtils.displayPanel(panelSize, "Scheme", "desc", "src/main/java/org/yourcompany/yourproject/resources/logo.jpeg");
+                    panel.setBounds(bounds);
+                    panel.setVisible(true);
+                    return panel;
+                }
+                case 36: {
+                    JPanel panel = PanelUtils.displayPanel(panelSize, "Common LISP", "desc", "src/main/java/org/yourcompany/yourproject/resources/logo.jpeg");
+                    panel.setBounds(bounds);
+                    panel.setVisible(true);
+                    return panel;
+                }
+                case 37: {
+                    JPanel panel = PanelUtils.displayPanel(panelSize, "SNOBOL", "desc", "src/main/java/org/yourcompany/yourproject/resources/logo.jpeg");
+                    panel.setBounds(bounds);
+                    panel.setVisible(true);
+                    return panel;
+                }
+                case 38: {
+                    JPanel panel = PanelUtils.displayPanel(panelSize, "ICON", "desc", "src/main/java/org/yourcompany/yourproject/resources/logo.jpeg");
+                    panel.setBounds(bounds);
+                    panel.setVisible(true);
+                    return panel;
+                }
                 default:
                     throw new AssertionError();
             }
